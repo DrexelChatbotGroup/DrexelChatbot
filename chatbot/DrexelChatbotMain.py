606c83be4105a8088a9ab54d633b450831faa182
shishir <sk3432@drexel.edu>
2017-02-21 14:58:35 -0500

    added class skeleton for GenericAnswerPopulation

---
+++ chatbot/DrexelChatbotMain.py
@@ -0,0 +1,12 @@
+from chatbot.GenericAnswerPopulation import GenericAnswerPopulation
+
+def main():
+
+    print("hello world")
+
+    #test
+    gac = GenericAnswerPopulation()
+    gac.populate()
+
+if __name__ == '__main__':
+    main()
