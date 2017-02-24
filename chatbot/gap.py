75c4fcaf8f4bca1fda502ba6c6f8bf352285f6c1
shishir <sk3432@drexel.edu>
2017-02-24 15:46:20 -0500

    added main code for gap, it does not handle exceptions yet

--- chatbot/gap.py
+++ chatbot/gap.py
@@ -1,25 +1,53 @@
+import re
+
 class GenericAnswerPopulation:
-    def __init__(self, genericAnswer, db):
-        self.genericAnswer = "nothing here"
-        self.dictionary1 = "nothing here"
-        self.dictionary2 = "nothing here"
-        self.combinedDictionary = "nothing here"
+    def __init__(self, _genericAnswer, db):
+        self.genericAnswer = _genericAnswer["answer"]
+        self.dictionary1 = _genericAnswer["dictionary1"]
+        self.query = _genericAnswer["query"]
+        self.dictionary2 = _genericAnswer["dictionary2"]
+        self.db = db;
 
     def populate(self):
-        print("not implemented yet")
+        query = self.__populateQuery(self.query, self.dictionary1)
+        dictionary2 = self.__queryDatabase(query)
+        combinedDictionary = self.__combineDictionary(self.dictionary1, dictionary2)
+        answer = self.__populateFromDictionary(self.genericAnswer, combinedDictionary)
+        return answer
+
+    def __queryDatabase(self, query):
+        #some database magic here
+        #db.query(query)
+        #return result as dictionary
+        return self.dictionary2
 
 
-    def queryDatabase(self):
-        print("not implemented yet")
+    def __combineDictionary(self, dictionary1, dictionary2):
+        combinedDictionary = self.dictionary1.copy()
+        combinedDictionary.update(dictionary2)
+        return combinedDictionary
 
 
-    def combineDictionary(self):
-        print("not implemented yet")
+    def __populateFromDictionary(self, genericAnswer, dictionary):
+        ans = self.__getWordsInsideParenthesis(genericAnswer)
+        length = len(ans);
+        for i in range(0,length):
+            key = ans[i]
+            genericAnswer = genericAnswer.replace("(" + ans[i] + ")", dictionary[key])
+        print("[final answer populated] " +  genericAnswer)
+        return genericAnswer
 
+    def __getWordsStartingWithDollar(self, sentence):
+        return [ t for t in sentence.split() if t.startswith('$') ]
 
-    def populateFromDictionary(self):
-        print("not implemented yet")
+    def __getWordsInsideParenthesis(self, sentence):
+        return re.findall(r'\((.*?)\)',sentence)
 
-
-    def populateQuery(self):
-        print("not implemented yet")
+    def __populateQuery(self, query, dictionary):
+        ans = self.__getWordsInsideParenthesis(query)
+        length = len(ans);
+        for i in range(0,length):
+            key = ans[i]
+            query = query.replace("(" + ans[i] + ")", dictionary[key]);
+        print("[query populated] " + query)
+        return query
