5927d89c92992de0c1eacf62166a868dab3d1727
hoathaivu <vthoa_25@yahoo.com>
2017-02-27 20:28:59 -0500

    fill out all methods

--- GenericQuestionConstruction.py
+++ GenericQuestionConstruction.py
@@ -9,18 +9,28 @@ class GenericQuestionConstruction():
 
 	def getGenericQuestion(self):
 		self.tagList = getPOSTag(self.question)
-		nounList = []
-		for tup in self.tagList:
-			if tup[1][0].lower() is 'n':
-				nounList.append(tup)
 		repList = findRepresentation()
+		#replace nouns with their genericRepresentations
 		paddedQuestion = self.question
 		for key, value in repList.items():
 			paddedQuestion.replace(key, value)
+		#create returned object
 		returnedObject = QuestionConstruction()
 		returnedObject.paddedQuestion = paddedQuestion
 		returnedObject.repList = repList
 		return returnedObject
 
 	def findRepresentation():
-
+		#retrieve all nouns
+		nounList = []
+                for tup in self.tagList:
+                        if tup[1][0].lower() is 'n':
+                                nounList.append(tup[0])
+		repList = {}
+		for noun in nounList:
+			rep = ""
+			#get genericRepresentation
+			
+			#store tuples
+			repList[noun] = rep
+		return repList
