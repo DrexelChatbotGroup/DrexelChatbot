663ca44b3bb9823e7146ce7693e3a89c916525b4
tpa27 <tpa27@drexel.edu>
2017-03-07 15:59:30 -0500

    Added remove function

--- database/stardog.py
+++ database/stardog.py
@@ -1,3 +1,11 @@
+"""
+Wrapper class and functions for interacting with a stardog database.
+"""
+
+__all__ = ["StardogDB"]
+__version__ = '0.1'
+__author__ = 'Tom Amon and Nanxi Zhang'
+
 import subprocess
 
 _STARDOG_INSTALL_PATH = "/home/stardog/bin/"
@@ -8,11 +16,16 @@ class StardogDB:
     def __init__(self, database_name):
         self._database_name = database_name
 
+    def remove_all(self):
+        #./stardog data add db filename.ttl
+        path = _STARDOG_INSTALL_PATH + "stardog"
+        command = [path, "data", "remove", "-a"]
+        subprocess.call(command)
+
     def add(self, ttl_files):
         #./stardog data add db filename.ttl
         path = _STARDOG_INSTALL_PATH + "stardog"
         command = [path, "data", "add", self._database_name]
-        print(command + ttl_files)
         subprocess.call(command + ttl_files)
          
     def query(self, str_query):
@@ -20,7 +33,7 @@ class StardogDB:
         path = _STARDOG_INSTALL_PATH + "stardog"
         out = subprocess.check_output([path, "query", self._database_name, str_query])
         value = str(out).split("|")[3].strip("\" ")
-        print(value)
+        return value
 
 
 if __name__ == "__main__":
