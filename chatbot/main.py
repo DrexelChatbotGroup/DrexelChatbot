17f089e7842b574bddc14972eb88a5b330a8eb7f
tpa27 <tpa27@drexel.edu>
2017-03-09 20:02:10 -0500

    Replace tabs with spaces

--- chatbot/main.py
+++ chatbot/main.py
@@ -6,22 +6,21 @@ import chatbot.gap
 from database.stardog import StardogDB
 
 def _main(question):
-	try :
-		#need to get question from website
-		#question = "test"
-		db = StardogDB("chatbotDB")
-		config = "genericAnswers.txt"
-		answer = None
+    try :
+        #need to get question from website
+        db = StardogDB("chatbotDB")
+        config = "genericAnswers.txt"
+        answer = None
 
-		gqc_object = gqc.GenericQuestionConstruction(question, db)
-		genericquestion = gqc_object.getgenericquestion()
+        gqc_object = gqc.GenericQuestionConstruction(question, db)
+        genericquestion = gqc_object.getgenericquestion()
 
-		gac_object = gac.GenericAnswerConstruction(config, answer)
-		genericanswer = gac_object.generateGenericAnswer(genericquestion.paddedquestion)
+        gac_object = gac.GenericAnswerConstruction(config, answer)
+        genericanswer = gac_object.generateGenericAnswer(genericquestion.paddedquestion)
 
-		#expected genericAnswer is different from one being created
-		gap_object = gap.GenericAnswerPopulation(genericAnswer, db)
-		answer = gap_object.populate()
+        #expected genericAnswer is different from one being created
+        gap_object = gap.GenericAnswerPopulation(genericAnswer, db)
+        answer = gap_object.populate()
 
     except ChatbotException as ex:
         ErrorHandler.handle(ex)
