70ec4b710b96e84c83657d7690c1434b1c234a3f
shishir <sk3432@drexel.edu>
2017-03-11 15:12:47 -0500

    added error messages when handling exception

--- chatbot/errors.py
+++ chatbot/errors.py
@@ -2,11 +2,11 @@
 class ErrorHandler():
 	def handle(ex):
 		if type(ex) is BadQuestionException:
-			print("V V V SAD! ")
+			print("Sorry I did not understand your question. I am just a bot and I am not as smart as you. Maybe someday I will be smarter and rule your world.")
 		elif type(ex) is BadAnswerException:
-			print("JUST V SAD! ")
+			print("Sorry I don't have an answer for that. I could give you a link to the website which might have the answer but my programmers will implement it next quarter.")
 		else:
-			print("BAD")
+			print("I don't know what happened. I have error in my system and the programmers did not catch that error. They are not cool.")
 
 class ChatbotException(BaseException):
 	"""Base class for exceptions of the Drexel Chatbot"""
