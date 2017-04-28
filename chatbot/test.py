9ed1065a8ea04eb4f9b3572f8ae2b9298d2758ea
hoathaivu <vthoa_25@yahoo.com>
2017-04-27 22:31:45 -0400

    add directory for documents

--- chatbot/test.py
+++ chatbot/test.py
@@ -1,24 +1,65 @@
 from postag import NLTKPOSTag
+import sys
 
+#sentence = sys.argv[1]
+#a = NLTKPOSTag()
+#b = a.getpostag(sentence)
+#
+#noun_list = []
+#count = 0
+#while count < len(b):
+#    word = b[count][0]
+#    tag = b[count][1]
+#    if tag[0].lower() == 'n':
+#        noun = word
+#        #consider adjacent proper nouns to be 1 noun
+#        if tag[:3].lower() == 'nnp':
+#            while count + 1 < len(b):
+#                count = count + 1
+#                word = b[count][0]
+#                tag = b[count][1]
+#                if tag[:3].lower() == 'nnp':
+#                    noun = noun + ' ' + word
+#                else:
+#                    count = count - 1
+#                    break
+#        noun_list.append(noun)
+#    count = count + 1
+#print(noun_list)
+
+filename = 'gac_data_yesyes.txt'
+try:
+    text = open(filename, 'rb')
+except OSError:
+    print('Error opening file')
+    exit()
+sentences = []
+for line in text:
+    sentences.append(line.strip())
+bs = []
 a = NLTKPOSTag()
-#b = a.getpostag('What is professor Mongan\'s email address?')
-#b = a.getpostag('Who is Yi Deng?')
-b = a.getpostag('Email Building Room is not Mongan?')
-print(b)
+for sentence in sentences:
+    bs.append(a.getpostag(sentence))
 
-noun_list = []
-count = 0
-while count < len(b):
-    tup = b[count]
-    if tup[1][0].lower() == 'n':
-        noun = tup[0]
-        if tup[1][:3].lower() == 'nnp':
-            while count + 1 < len(b):
-                count = count + 1
-                if b[count][1][:3].lower() == 'nnp':
-                    noun = noun + ' ' + b[count][0]
-                else:
-                    break
-        noun_list.append(noun)
-    count = count + 1
-print(noun_list)
+for b in bs:
+    noun_list = []
+    count = 0
+    while count < len(b):
+        word = b[count][0]
+        tag = b[count][1]
+        if tag[0].lower() == 'n':
+            noun = word
+            #consider adjacent proper nouns to be 1 noun
+            if tag[:3].lower() == 'nnp':
+                while count + 1 < len(b):
+                    count = count + 1
+                    word = b[count][0]
+                    tag = b[count][1]
+                    if tag[:3].lower() == 'nnp':
+                        noun = noun + ' ' + word
+                    else:
+                        count = count - 1
+                        break
+            noun_list.append(noun)
+        count = count + 1
+    print(noun_list)
