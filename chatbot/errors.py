dda46ea315d6bce5058d092932b5e54542b13275
hoathaivu <vthoa_25@yahoo.com>
2017-05-07 23:07:28 -0400

    attempt to fix ErrorHandler not running

--- chatbot/errors.py
+++ chatbot/errors.py
@@ -1,19 +1,20 @@
 
 class ErrorHandler():
-    def handle(ex):
+    @classmethod
+    def handle(clsobj, ex):
         if type(ex) is BadQuestionException:
             print("Sorry I did not understand your question. I am just a bot and I am not as smart as you. Maybe someday I will be smarter and rule your world.")
         elif type(ex) is BadAnswerException:
             print("Sorry I don't have an answer for that. I could give you the link(s) to website(s) which might have the answer:")
-            query = "prefix cb: <http://drexelchatbot.com/rdf/> select ?website where { ?s cb:name \"(target)\" . ?s cb:website ?website}"
+            query = """prefix cb: <http://drexelchatbot.com/rdf/> select ?website where { ?s cb:name \"(target)\" . ?s cb:website ?website}"""
             for key in ex.dictionary.keys():
-                new_query = query.replace("(target)", dictionary[key])
+                new_query = query.replace("(target)", ex.dictionary[key])
                 returned_dict = ex.db.query(new_query)
-                if key is in returned_dict:
-                    website = returned_dict[key]
-                    print("%s: %s" % (dictionary[key], website))
+                if returned_dict is not None:
+                    website = returned_dict['website']
+                    print("%s: %s" % (ex.dictionary[key], website))
                 else:
-                    print("My apology. We don't have a website for %s" % dictionary[key])
+                    print("My apology. We don't have a website for %s" % ex.dictionary[key])
         else:
             print("I don't know what happened. I have error in my system and the programmers did not catch that error. They are not cool.")
 
--- chatbot/test.py
+++ chatbot/test.py
@@ -1,65 +1,9 @@
-from postag import NLTKPOSTag
+from errors import *
 import sys
 
-#sentence = sys.argv[1]
-#a = NLTKPOSTag()
-#b = a.getpostag(sentence)
-#
-#noun_list = []
-#count = 0
-#while count < len(b):
-#    word = b[count][0]
-#    tag = b[count][1]
-#    if tag[0].lower() == 'n':
-#        noun = word
-#        #consider adjacent proper nouns to be 1 noun
-#        if tag[:3].lower() == 'nnp':
-#            while count + 1 < len(b):
-#                count = count + 1
-#                word = b[count][0]
-#                tag = b[count][1]
-#                if tag[:3].lower() == 'nnp':
-#                    noun = noun + ' ' + word
-#                else:
-#                    count = count - 1
-#                    break
-#        noun_list.append(noun)
-#    count = count + 1
-#print(noun_list)
-
-filename = 'gac_data_yesyes.txt'
 try:
-    text = open(filename, 'rb')
-except OSError:
-    print('Error opening file')
-    exit()
-sentences = []
-for line in text:
-    sentences.append(line.strip())
-bs = []
-a = NLTKPOSTag()
-for sentence in sentences:
-    bs.append(a.getpostag(sentence))
-
-for b in bs:
-    noun_list = []
-    count = 0
-    while count < len(b):
-        word = b[count][0]
-        tag = b[count][1]
-        if tag[0].lower() == 'n':
-            noun = word
-            #consider adjacent proper nouns to be 1 noun
-            if tag[:3].lower() == 'nnp':
-                while count + 1 < len(b):
-                    count = count + 1
-                    word = b[count][0]
-                    tag = b[count][1]
-                    if tag[:3].lower() == 'nnp':
-                        noun = noun + ' ' + word
-                    else:
-                        count = count - 1
-                        break
-            noun_list.append(noun)
-        count = count + 1
-    print(noun_list)
+    db = ''
+    dictionary = {'target':'test'}
+    raise BadAnswerException(db, dictionary)
+except ChatbotException as ex:
+    ErrorHandler.handle(ex)
