20e3358169b76a7ba03e11c9a4d475b28f51c6e7
hoa712 <vthoa_25@yahoo.com>
2017-05-07 23:08:44 -0400

    Delete test.py

--- chatbot/test.py
+++
@@ -1,9 +0,0 @@
-from errors import *
-import sys
-
-try:
-    db = ''
-    dictionary = {'target':'test'}
-    raise BadAnswerException(db, dictionary)
-except ChatbotException as ex:
-    ErrorHandler.handle(ex)
