8fc57baa175770a87acd032ad3635eb4d7db5910
hoathaivu <vthoa_25@yahoo.com>
2017-05-09 21:51:59 -0400

    add logging

--- chatbot/gap.py
+++ chatbot/gap.py
@@ -11,6 +11,7 @@ class GenericAnswerPopulation:
         self.db = db;
 
     def populate(self, gqc_dictionary):
+        logging.debug("Running Generic Answer Population...")
         self.__populateQuery(gqc_dictionary)
         ans_dictionary = self.__queryDatabase()
         combinedDictionary = self.__combineDictionary(gqc_dictionary, ans_dictionary)
@@ -19,9 +20,10 @@ class GenericAnswerPopulation:
 
     def __queryDatabase(self):
         ans_dictionary = self.db.query(self.query)
+        logging.debug("queried answer dictionary: " + str(ans_dictionary))
         if(len(ans_dictionary) == 0):
-            logging.error("Error!!! The database query returned empty dictionary")
-            logging.error("The query was:\n" +  self.query);
+            logging.warning("Error!!! The database query returned empty dictionary")
+            logging.warning("The query was:\n" +  self.query);
             pass
         return ans_dictionary
 
@@ -29,19 +31,20 @@ class GenericAnswerPopulation:
     def __combineDictionary(self, dictionary1, dictionary2):
         combinedDictionary = dictionary1.copy()
         combinedDictionary.update(dictionary2)
+        logging.debug("combined dictionary: " + str(combinedDictionary))
         return combinedDictionary
 
 
     def __populateFromDictionary(self, dictionary):
         rep_list = self.__getWordsInsideParenthesis(self.genericAnswer)
+        logging.debug("rep_list: " + str(rep_list))
         genericAnswer = self.genericAnswer
         for rep in rep_list:
                 if not(rep in dictionary):
-                    logging.error("Error!!! when populating final answer")
+                    logging.warning("Error!!! when populating final answer")
                     raise BadAnswerException(db, dictionary)
                 genericAnswer = genericAnswer.replace("(" + rep + ")", dictionary[rep])
 
-        logging.info("final answer populated: " +  genericAnswer + "\n")
         return genericAnswer
 
     def __getWordsStartingWithDollar(self, sentence):
@@ -51,11 +54,12 @@ class GenericAnswerPopulation:
         return re.findall(r'\((.*?)\)',sentence)
 
     def __populateQuery(self, dictionary):
+        logging.debug("question dictionary: " + str(dictionary))
         var_list = self.__getWordsInsideParenthesis(self.query)
+        logging.debug("var_list: " + str(var_list))
         for key in var_list:
             if not (key in dictionary):
-                logging.error("Error!!! when constructing query from generic query")
+                logging.warning("Error!!! when constructing query from generic query")
                 raise BadQuestionException()
             self.query = self.query.replace("(" + key + ")", dictionary[key]);
-
-        logging.info("query populated: " + self.query)
+        logging.debug("query populated: " + self.query)
