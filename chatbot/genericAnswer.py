b50c3b19eef94561715dfaccec6d3bd83b6396fe
Daniel Fitzick <dwf35@drexel.edu>
2017-05-10 16:29:22 +0000

    Synching from server to repo. (also 200th commit)

---
+++ chatbot/genericAnswer.py
@@ -0,0 +1,11 @@
+class GenericAnswer:
+    def __init__(self, newAnswer, newQuery):
+        self.answer = newAnswer
+        self.query = newQuery
+
+    def getAnswer(self):
+        return self.answer
+
+    def getQuery(self):
+        return self.query
+
